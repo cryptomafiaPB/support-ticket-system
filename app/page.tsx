e2f9c4863b9a93f32749d3f9a7b3676eb8934cb3
cryptomafiaPB <jugad360@gmail.com>
2025-02-06 00:38:50 +0530

    Update homepage styling for improved responsiveness and readability

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold mb-4">
+    <main className="min-h-screen flex flex-col items-center justify-center mx-8 sm:mx-auto">
+      <h1 className="text-2xl sm:text-4xl font-bold mb-4 text-center">
         Welcome to the Ticket Support System
       </h1>
       <Link
